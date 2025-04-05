56b2a48109e3a8d7a50c314f71102a4c7f36e6cc
Dario616 <alfacentauri818@gmail.com>
2025-04-05 18:40:00 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'tickets-soporte',
+    loadChildren: () => import('./tickets-soporte/tickets-soporte.module').then( m => m.TicketsSoportePageModule)
   }
 ];
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCRuFfVPgW7oP5PmVx2C5xVk60-8lLSajw",
+    authDomain: "tickets-project-df47d.firebaseapp.com",
+    projectId: "tickets-project-df47d",
+    storageBucket: "tickets-project-df47d.firebasestorage.app",
+    messagingSenderId: "964754975414",
+    appId: "1:964754975414:web:38a6a6fade96bbec9a1417"
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
